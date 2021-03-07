a0b200aa303450e225d613021fed4fb821f1510d
leaonidas <mtmleao@gmail.com>
2021-03-07 18:00:15 +0000

    some stuff

--- components/global/slideshow.js
+++ components/global/slideshow.js
@@ -1,15 +1,26 @@
+import Slider from 'react-slick';
+
 export default function Slideshow() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 5000
+  };
   return (
-    <div className="slideshow">
+    <Slider {...settings}>
       <div className="slideshow__slide">
         <img src="/images/slideshow/first.jpg" alt="Construction sight" />
       </div>
-      {/* <div className="slideshow__slide">
-            <img src="/images/slideshow/first.jpg" alt="Construction sight" />
-          </div>
-          <div className="slideshow__slide">
-            <img src="/images/slideshow/first.jpg" alt="Construction sight" />
-          </div> */}
-    </div>
+      <div className="slideshow__slide">
+        <img src="/images/slideshow/second.jpg" alt="Construction sight" />
+      </div>
+      <div className="slideshow__slide">
+        <img src="/images/slideshow/third.jpg" alt="Construction sight" />
+      </div>
+    </Slider>
   );
 }
