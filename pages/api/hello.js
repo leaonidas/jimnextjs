3f6f07e86213187a33b80e70ccafeb969ebf831d
João Gonçalves <joaogoncalves@pixelmatters.com>
2021-03-23 18:53:37 +0000

    Remove api folder

--- pages/api/hello.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (_request, response) => {
-  response.statusCode = 200;
-  response.json({ name: 'John Doe' });
-};
